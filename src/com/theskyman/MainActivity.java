abe94cf1c023d0f1c8fb52cc0f5decba9ffcea99
3legcat <woolpee@gmail.com>
2013-08-03 14:40:04 +0800

    add box2d demo

--- src/com/theskyman/MainActivity.java
+++ src/com/theskyman/MainActivity.java
@@ -1,36 +1,41 @@
 package com.theskyman;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 	
 	 public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	 public final static String[] items = {"box2d"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
     }
 
 
     @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	     Intent intent = new Intent(this, Box2dActivity.class);
+
+	     startActivity(intent);
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
-    
-    public void sendMessage(View view){
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
 }
